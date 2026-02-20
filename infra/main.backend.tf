4affb53710068a99fe067c7cc4aeb19f78793221
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-02-20 14:20:22 +0100

    refactor infrastructure: add PostgreSQL flexible server and database resources with firewall rule

--- infra/main.backend.tf
+++ infra/main.backend.tf
@@ -23,3 +23,35 @@ resource "azurerm_linux_web_app" "todo-app-as" {
     type = "SystemAssigned"
   }
 }
+
+resource "azurerm_postgresql_flexible_server" "postgres" {
+  name                   = "todo-app-db"
+  resource_group_name    = azurerm_resource_group.rg-todo-app-dev.name
+  location               = azurerm_resource_group.rg-todo-app-dev.location
+  version                = "15"
+
+  administrator_login    = var.administrator_login
+  administrator_password = var.administrator_password
+
+  sku_name   = "B_Standard_B1ms"
+  storage_mb = 32768
+
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+
+  zone = "1"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "todo-app-db" {
+  name      = "todo-app-db"
+  server_id = azurerm_postgresql_flexible_server.postgres.id
+  collation = "en_US.utf8"
+  charset   = "UTF8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure" {
+  name             = "allow-azure-services"
+  server_id        = azurerm_postgresql_flexible_server.postgres.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,13 @@ variable "subscription_id" {
     description = "logical container id"
     type = string
 }
+
+variable "administrator_login" {
+    description = "administration login"
+    type = string
+}
+
+variable "administrator_password" {
+    description = "administration password"
+    type = string
+}
