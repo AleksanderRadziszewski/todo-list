406a6c6e40cd6a83061ff1cf7ae9c13be56c4e4f
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-03-26 14:00:01 +0100

    refactor key vault: add SWA deployment token secret to key vault configuration

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -3,3 +3,13 @@ resource "azurerm_key_vault_secret" "app_secrets" {
   key_vault_id = azurerm_key_vault.my_todo_app_kv.id
   value        = random_password.db_password.result
 }
+
+resource "azurerm_key_vault_secret" "swa_token" {
+  name         = "swa-deployment-token"
+  value        = azurerm_static_web_app.todo_app_frontend.api_key
+  key_vault_id = azurerm_key_vault.my_todo_app_kv.id
+
+  depends_on = [
+    azurerm_static_web_app.todo_app_frontend
+  ]
+}
