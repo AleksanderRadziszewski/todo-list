69b6178fc122b1f84917401d1e71608b871abdbc
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-03-26 15:04:09 +0100

    refactor key vault: standardize secret names to uppercase for consistency

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,11 +1,11 @@
 resource "azurerm_key_vault_secret" "app_secrets" {
-  name         = "db-password"
+  name         = "DB-PASSWORD"
   key_vault_id = azurerm_key_vault.my_todo_app_kv.id
   value        = random_password.db_password.result
 }
 
 resource "azurerm_key_vault_secret" "swa_token" {
-  name         = "swa-deployment-token"
+  name         = "SWA-DEPLOYMENT-TOKEN"
   value        = azurerm_static_web_app.todo_app_frontend.api_key
   key_vault_id = azurerm_key_vault.my_todo_app_kv.id
 
