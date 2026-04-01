465a7d53b3fbc31053878e2ea7bc78c9032708f0
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-01 10:44:00 +0000

    docker_registry attributed droped

--- infra/backend.tf
+++ infra/backend.tf
@@ -15,8 +15,7 @@ resource "azurerm_linux_web_app" "todo_app_as" {
   site_config {
     always_on = false
     application_stack {
-      docker_image_name   = "todo-app-backend:latest"
-      docker_registry_url = "https://index.docker.io/"
+      docker_image_name = "todo-app-backend:latest"
     }
   }
   identity {
