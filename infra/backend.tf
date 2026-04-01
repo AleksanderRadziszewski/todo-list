a16f0ca1d1c87b240ff70a23e5340b5f408da1a9
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-01 10:03:31 +0000

    The service web app is kept continuously running

--- infra/backend.tf
+++ infra/backend.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "todo_app_as" {
   service_plan_id     = azurerm_service_plan.todo_app_service_plan_dev.id
 
   site_config {
-    always_on = false
+    always_on = true
     application_stack {
       docker_image_name   = "todo-app-backend:latest"
       docker_registry_url = "https://index.docker.io/"
