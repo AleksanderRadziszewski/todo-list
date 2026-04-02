1fa2dac14c737b78995917924938fc319b5f6f7f
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-02 14:00:37 +0200

    fix: update SKU name to B1 and enable always_on for web app; correct POSTGRES_PASSWORD variable name

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_secret" "app_secrets" {
-  name         = "POSTGRES-PASSWORD"
+  name         = "POSTGRES_PASSWORD"
   key_vault_id = azurerm_key_vault.my_todo_app_kv.id
   value        = random_password.db_password.result
 }
