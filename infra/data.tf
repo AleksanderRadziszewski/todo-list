8a9b9bffebd5b1b4e6f5c18984d2e232f1a73fd2
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-03 17:04:16 +0000

    POSTGRES_PASSWORD secret download from the my-todo-app-kv

--- infra/data.tf
+++ infra/data.tf
@@ -8,3 +8,8 @@ data "azurerm_key_vault" "my_todo_app_kv" {
 data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
+
+data "azurerm_key_vault_secret" "app_secrets" {
+  name         = "POSTGRES-PASSWORD"
+  key_vault_id = azurerm_key_vault.my_todo_app_kv.id
+}
