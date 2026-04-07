222824e22e985a8fbb1e6f2386a037481d475197
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-07 12:50:16 +0200

    fix: correct data source reference for API_BASE_URL in frontend configuration

--- infra/data.tf
+++ infra/data.tf
@@ -2,3 +2,8 @@ data "azurerm_client_config" "current" {}
 data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
+
+data "azurerm_linux_web_app" "todo_app_as" {
+  name                = "todo-app-as"
+  resource_group_name = "todo-rg"
+}
