780ff88be3f98c0ba97f9d0113a2be1f120db66b
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-07 13:07:54 +0200

    fix: update resource type for todo_app_as and correct API_BASE_URL reference in frontend configuration

--- infra/data.tf
+++ infra/data.tf
@@ -3,7 +3,7 @@ data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
 
-data "azurerm_linux_web_app" "todo_app_as" {
+data "azurerm_app_service" "todo_app_as" {
   name                = "todo-app-as"
   resource_group_name = "todo-rg"
 }
