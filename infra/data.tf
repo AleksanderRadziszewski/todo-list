42003a377589720c3cd148b5a50c78e9d2721480
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-10 16:52:28 +0200

    app service data source deleted

--- infra/data.tf
+++ infra/data.tf
@@ -2,8 +2,3 @@ data "azurerm_client_config" "current" {}
 data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
-
-data "azurerm_linux_web_app" "todo_app_as" {
-  name                = azurerm_linux_web_app.todo_app_as.name
-  resource_group_name = azurerm_linux_web_app.todo_app_as.resource_group_name
-}
