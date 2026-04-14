1eb87241f81f96216e5fc8d7ea10d8a787a768c0
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-14 19:14:02 +0200

    backend config names, storage account name modified. Checkout in deploy stage

--- infra/infra.init.sh
+++ infra/infra.init.sh
@@ -2,7 +2,7 @@ az group create --name rg-todo-app --location westeurope
 
 az storage account create \
   --resource-group rg-todo-app \
-  --name tfstatetodoapp \
+  --name tfstate-todo-app \
   --sku Standard_LRS \
   --encryption-services blob
 
