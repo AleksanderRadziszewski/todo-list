3c7f74295aee4d5cb15c338135896ae95dfb6b2d
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-17 15:09:10 +0200

    swa and rg creation

--- infra/data.tf
+++ infra/data.tf
@@ -3,7 +3,7 @@ data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
 
-data "azurerm_static_web_app" "frontend" {
+data "azurerm_static_web_app" "todo_app_frontend" {
   name = "todo-app-${var.environment}-frontend-static"
   resource_group_name = "rg-todo-app-${var.environment}"
 }
