6ca4c00fca1cfa3b88178393ee2e22cd33b48fc7
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-04-17 13:55:47 +0200

    planning and applying SWA, data source for default swa hostname

--- infra/data.tf
+++ infra/data.tf
@@ -2,3 +2,8 @@ data "azurerm_client_config" "current" {}
 data "azuread_service_principal" "ado" {
   display_name = "todo-app-sp-pipeline"
 }
+
+data "azurerm_static_web_app" "frontend" {
+  name = "deafult url"
+  resource_group_name = "rg-todo-app-${var.environment}"
+}
