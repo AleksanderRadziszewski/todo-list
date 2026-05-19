08f7270138098273b8d03e790c0a2856c9e2b73f
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-05-19 19:31:53 +0200

    fix: update PostgreSQL password secret reference in configuration

--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault_secret" "app_secrets" {
+resource "azurerm_key_vault_secret" "postgres_password" {
   name         = "POSTGRES-PASSWORD"
   key_vault_id = azurerm_key_vault.my_todo_app_kv.id
   value        = random_password.db_password.result
