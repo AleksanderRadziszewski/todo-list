e1f48dcf9752f246a03c53a225ee4cc5ef6c59da
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2026-06-03 14:08:49 +0200

    witFor type deleted, setupTests.ts modified

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,17 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// Polyfill import.meta for Jest testing
-if (!global.import) {
-  (global as any).import = {};
-}
-if (!(global as any).import.meta) {
-  (global as any).import.meta = {
+(global as any).import = {
+  meta: {
     env: {
       VITE_API_BASE_URL: 'api.example.com',
     },
-  };
-}
+  },
+};
